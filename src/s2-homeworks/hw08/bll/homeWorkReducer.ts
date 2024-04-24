a31e9d2fcad47d8a6a15e980f47327a7db71b046
julia-lahtina <j.lakhtin@gmail.com>
2024-04-24 19:38:20 +0200

    HW8

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -4,15 +4,36 @@ type ActionType =
     | { type: 'sort'; payload: 'up' | 'down' }
     | { type: 'check'; payload: number }
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+export const homeWorkReducer = (state: UserType[], action: ActionType): UserType[] => { // need to fix any
     switch (action.type) {
         case 'sort': { // by name
 
-            return state // need to fix
+            return [...state].sort(function (a, b) {
+                if (action.payload === 'up') {
+                    if (a.name < b.name) {
+                        return -1;
+                    }
+                    if (a.name > b.name) {
+                        return 1;
+                    }
+                    return 0;
+                }
+                if (action.payload === 'down') {
+                    if (a.name < b.name) {
+                        return 1;
+                    }
+                    if (a.name > b.name) {
+                        return -1;
+                    }
+                    return 0;
+                }
+                return 0;
+            })
         }
         case 'check': {
 
-            return state // need to fix
+            // return state // need to fix
+            return [...state].filter(p => p.age > 18)
         }
         default:
             return state
