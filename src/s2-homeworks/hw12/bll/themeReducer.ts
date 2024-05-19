20c2736103d7080af1e41a43617f4af56120c80b
julia-lahtina <j.lakhtin@gmail.com>
2024-05-19 11:56:17 +0200

    fix hw12

--- src/s2-homeworks/hw12/bll/themeReducer.ts
+++ src/s2-homeworks/hw12/bll/themeReducer.ts
@@ -19,3 +19,4 @@ export const themeReducer = (state: initStateType = initState, action: changeThe
 export const changeThemeId = (id: number) => ({ type: 'SET_THEME_ID', id } as const) // fix any
 
 type changeThemeIdType = ReturnType<typeof changeThemeId>
+вв
