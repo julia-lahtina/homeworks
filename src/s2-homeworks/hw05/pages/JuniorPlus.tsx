ea5b3dbe58849470be446240a3d99078836ab449
julia-lahtina <j.lakhtin@gmail.com>
2024-05-28 20:36:31 +0200

    HW13

--- src/s2-homeworks/hw05/pages/JuniorPlus.tsx
+++ src/s2-homeworks/hw05/pages/JuniorPlus.tsx
@@ -1,7 +1,8 @@
-import React from 'react'
-import HW10 from '../../hw10/HW10';
-import HW11 from '../../hw11/HW11';
-import HW12 from '../../hw12/HW12';
+import React from "react";
+import HW10 from "../../hw10/HW10";
+import HW11 from "../../hw11/HW11";
+import HW12 from "../../hw12/HW12";
+import HW13 from "../../hw13/HW13";
 // import HW10 from '../../hw10/HW10'
 // import HW11 from '../../hw11/HW11'
 // import HW12 from '../../hw12/HW12'
@@ -10,16 +11,16 @@ import HW12 from '../../hw12/HW12';
 // import HW15 from '../../hw15/HW15'
 
 function JuniorPlus() {
-    return (
-        <div id={'hw5-page-junior-plus'}>
-            <HW10 />
-            <HW11 />
-            <HW12 />
-            {/*<HW13 />*/}
-            {/*<HW14 />*/}
-            {/*<HW15 />*/}
-        </div>
-    )
+  return (
+    <div id={"hw5-page-junior-plus"}>
+      <HW10 />
+      <HW11 />
+      <HW12 />
+      <HW13 />
+      {/*<HW14 />*/}
+      {/*<HW15 />*/}
+    </div>
+  );
 }
 
-export default JuniorPlus
+export default JuniorPlus;
